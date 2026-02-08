399dddde236050717d428bc9808324ddb58379a9
npdr <p.nieuwenhoff@gmail.com>
2026-02-08 19:31:35 +0100

    oil hidden files

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -9,4 +9,12 @@ return {
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
+  config = function()
+	  require("oil").setup({
+  view_options = {
+    show_hidden = true, -- Show hidden files by default
+  },
+})
+
+  end
 }
